02ad1667aa22bed285e193ca2eca3d60cdce3536
Joana Reis <joanarfreis@gmail.com>
2018-05-29 13:15:00 +0100

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :chickens_gangs
+  resources :chickens_gangs do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
