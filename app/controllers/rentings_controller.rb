f22f9d8edb732933b7665230c33d93538daddf03
catarinacota <catarina.afc@gmail.com>
2018-05-30 10:24:39 +0100

    renting show page

--- app/controllers/rentings_controller.rb
+++ app/controllers/rentings_controller.rb
@@ -1,6 +1,10 @@
 class RentingsController < ApplicationController
   before_action :set_chickens_gang
 
+  def show
+    @renting = Renting.find(params[:id])
+  end
+
   def new
     @renting = Renting.new
   end
@@ -11,13 +15,13 @@ class RentingsController < ApplicationController
     @renting.user_id = @user
     @renting.chickens_gang = @chickens_gang
     @renting.save
-    redirect_to @chickens_gang
+    redirect_to chickens_gang_renting_path(@chickens_gang, @renting)
   end
 
   private
 
   def set_chickens_gang
-    @chickens_gang = ChickensGang.find(params[:id])
+    @chickens_gang = ChickensGang.find(params[:chickens_gang_id])
     authorize @chickens_gang
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :chickens_gangs do
-    member do
-      resources :rentings, only: [:show, :new, :create]
-    end
+    resources :rentings, only: [:show, :new, :create]
   end
 end
