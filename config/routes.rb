310e75c08c3a1c5b37063b143316d60a9834fd5a
Joana Reis <joanarfreis@gmail.com>
2018-05-30 13:36:05 +0100

    Working on farmers page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get'/farmers' => 'pages#farmers'
   resources :chickens_gangs do
     resources :rentings, only: [:show, :new, :create]
     resources :reviews, only: [:new, :create]
