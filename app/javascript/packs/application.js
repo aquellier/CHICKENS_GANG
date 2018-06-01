28ed2b0dbb45e27932728de24b26fa342ebce66a
diogo telo <diogo_telo@hotmail.com>
2018-06-01 15:59:56 +0100

    fixed map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,11 @@ if (homePage) {
 import { toggleDateInputs } from '../components/renting_form';
 toggleDateInputs();
 
+import initMap from '../components/map.js'
+const mapElement = document.getElementById('map');
+initMap(mapElement);
+console.log("Hello from app/javascript/packs/map.js!");
+
 
 //This is to change collor when you scrool but we didn't like it!!
 //import { toggleNavbarBackground } from '../components/navbar';
