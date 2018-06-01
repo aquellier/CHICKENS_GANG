c31ee5bd8b8f50a6c15980181a46edab1df4aa78
Antoine Quellier <antoinecquellier@gmail.com>
2018-06-01 13:27:50 +0100

    Chicken breeds done with link in the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get'/farmers' => 'pages#farmers'
+  get'/chickens_breeds' => 'pages#chickens_breeds'
   get'/profile' => 'pages#profile'
   #get "project/new_release" => 'project#new_release', :as => :new_release
   resources :chickens_gangs do
